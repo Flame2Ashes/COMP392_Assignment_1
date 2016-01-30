2fd05161ca9500e784bcfe3fea9470bab240ea8f
Angelina Gutierrez <angelina.ogutierrez@gmail.com>
2016-01-29 23:29:02 -0500

    Created game.ts file

---
+++ Scripts/core/game.js
@@ -0,0 +1,48 @@
+/// <reference path="_reference.ts"/>
+// MAIN GAME FILE
+// THREEJS Aliases
+var Scene = THREE.Scene;
+var Renderer = THREE.WebGLRenderer;
+var PerspectiveCamera = THREE.PerspectiveCamera;
+var BoxGeometry = THREE.BoxGeometry;
+var CubeGeometry = THREE.CubeGeometry;
+var PlaneGeometry = THREE.PlaneGeometry;
+var SphereGeometry = THREE.SphereGeometry;
+var Geometry = THREE.Geometry;
+var AxisHelper = THREE.AxisHelper;
+var LambertMaterial = THREE.MeshLambertMaterial;
+var MeshBasicMaterial = THREE.MeshBasicMaterial;
+var Material = THREE.Material;
+var Mesh = THREE.Mesh;
+var Object3D = THREE.Object3D;
+var SpotLight = THREE.SpotLight;
+var PointLight = THREE.PointLight;
+var AmbientLight = THREE.AmbientLight;
+var Control = objects.Control;
+var GUI = dat.GUI;
+var Color = THREE.Color;
+var Vector3 = THREE.Vector3;
+var Face3 = THREE.Face3;
+var Point = objects.Point;
+//Custom Game Objects
+var gameObject = objects.gameObject;
+var scene;
+var renderer;
+var camera;
+var axes;
+var cube;
+var plane;
+var sphere;
+var ambientLight;
+var spotLight;
+var control;
+var gui;
+var stats;
+var step = 0;
+var cubeGeometry;
+var cubeMaterial;
+function init() {
+    //Instantiate scene objects
+    scene = new Scene();
+}
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,54 @@
+/// <reference path="_reference.ts"/>
+
+// MAIN GAME FILE
+
+// THREEJS Aliases
+import Scene = THREE.Scene;
+import Renderer = THREE.WebGLRenderer;
+import PerspectiveCamera = THREE.PerspectiveCamera;
+import BoxGeometry = THREE.BoxGeometry;
+import CubeGeometry = THREE.CubeGeometry;
+import PlaneGeometry = THREE.PlaneGeometry;
+import SphereGeometry = THREE.SphereGeometry;
+import Geometry = THREE.Geometry;
+import AxisHelper = THREE.AxisHelper;
+import LambertMaterial = THREE.MeshLambertMaterial;
+import MeshBasicMaterial = THREE.MeshBasicMaterial;
+import Material = THREE.Material;
+import Mesh = THREE.Mesh;
+import Object3D = THREE.Object3D;
+import SpotLight = THREE.SpotLight;
+import PointLight = THREE.PointLight;
+import AmbientLight = THREE.AmbientLight;
+import Control = objects.Control;
+import GUI = dat.GUI;
+import Color = THREE.Color;
+import Vector3 = THREE.Vector3;
+import Face3 = THREE.Face3;
+import Point = objects.Point;
+
+//Custom Game Objects
+import gameObject = objects.gameObject;
+
+var scene: Scene;
+var renderer: Renderer;
+var camera: PerspectiveCamera;
+var axes: AxisHelper;
+var cube: Mesh;
+var plane: Mesh;
+var sphere: Mesh;
+var ambientLight: AmbientLight;
+var spotLight: SpotLight;
+var control: Control;
+var gui: GUI;
+var stats: Stats;
+var step: number = 0;
+var cubeGeometry:CubeGeometry;
+var cubeMaterial:LambertMaterial;
+
+function init() {
+    
+    //Instantiate scene objects
+    scene = new Scene();
+    
+}
